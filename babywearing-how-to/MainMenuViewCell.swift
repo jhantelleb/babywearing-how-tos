adbcef6e93944f86fd42d5646f2240e63ef19bbb
Jhantelleb <jhantelle.belleza@gmail.com>
2017-04-09 18:09:42 -0400

    Added Collection View-MVVM

---
+++ babywearing-how-to/MainMenuViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MainMenuViewCell.swift
+//  Babywearing
+//
+//  Created by Jhantelle Belleza on 4/9/17.
+//  Copyright © 2017 JhantelleB. All rights reserved.
+//
+
+import Foundation
---
+++ babywearing-how-to/MainMenuViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MainMenuViewModel.swift
+//  Babywearing
+//
+//  Created by Jhantelle Belleza on 4/8/17.
+//  Copyright © 2017 JhantelleB. All rights reserved.
+//
+
+import Foundation
