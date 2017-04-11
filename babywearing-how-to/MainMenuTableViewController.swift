2324990c4c5002fef66f2e46a99a17491946e0bb
Jhantelleb <jhantelle.belleza@gmail.com>
2017-04-11 18:37:20 -0400

    Generated test data

--- babywearing-how-to/MainMenuTableViewController.swift
+++ babywearing-how-to/MainMenuTableViewController.swift
@@ -16,7 +16,7 @@ class MainMenuTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        menu.generateMenuData()
+        menu.generateMenuData()
         
     }
         
@@ -28,19 +28,17 @@ class MainMenuTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-     
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     
         return menu.menuItems.count
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.mainMenuCell, for: indexPath)
-
+        cell.textLabel?.text = menu.menuItems[indexPath.row].description
         return cell
     }
 }
